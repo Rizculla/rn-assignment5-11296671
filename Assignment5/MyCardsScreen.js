a01f364f16559faa09b64c7152d46ab82041a85b
Rizculla <Biggybornchis123@gmail.com>
2024-06-26 11:29:42 +0000

    Added some dependencies and screens to my app

---
+++ Assignment5/MyCardsScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const MyCardsScreen = () => {
+  return (
+    <View>
+      <Text>My Cards Screen</Text>
+    </View>
+  );
+};
+
+export default MyCardsScreen;
+
